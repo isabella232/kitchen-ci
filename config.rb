787896056aa57f5b2c7190d492e952c2fcf6e976
Caleb Hailey <calebhailey@gmail.com>
2013-11-29 15:06:11 -0800

    first pieces of rewritten site navigation; still a bit more work to do to make it more dynamic (and derived from docs only, no helper classes)

--- config.rb
+++ config.rb
@@ -42,6 +42,7 @@ end
 # activate :automatic_image_sizes
 
 # Reload the browser automatically whenever files change
+activate :directory_indexes
 activate :livereload
 
 activate :syntax
@@ -50,48 +51,48 @@ set :markdown, fenced_code_blocks: true, smartypants: true
 
 # Methods defined in the helpers block are available in templates
 helpers do
-  def current_page?(page)
-    page.sub(%r{^/}, '') == request["path"]
-  end
+  # def current_page?(page)
+  #   page.sub(%r{^/}, '') == request["path"]
+  # end
 
-  def guide_nav(title, page)
-    path = "/getting-started/#{page}.html"
+  # def guide_nav(title, page)
+  #   path = "/getting-started/#{page}.html"
 
-    if current_page?(path)
-      %{<span class="glyphicon glyphicon-bookmark"></span> } + title
-    else
-      link_to(title, path)
-    end
-  end
+  #   if current_page?(path)
+  #     %{<span class="glyphicon glyphicon-bookmark"></span> } + title
+  #   else
+  #     link_to(title, path)
+  #   end
+  # end
 
-  def guide_sections
-    [ [ "introduction", "Introduction" ],
-      [ "installing", "Installing Test Kitchen" ],
-      [ "getting-help", "Getting Help" ],
-      [ "creating-cookbook", "Creating a Cookbook" ],
-      [ "writing-recipe", "Writing a Recipe" ],
-      [ "running-converge", "Running Kitchen Converge" ],
-      [ "manually-verifying", "Manually Verifying" ],
-      [ "writing-test", "Writing a Test" ],
-      [ "running-verify", "Running Kitchen Verify" ],
-      [ "running-test", "Running Kitchen Test" ],
-      [ "adding-platform", "Adding a Platform"  ],
-      [ "fixing-converge", "Fixing Converge" ],
-      [ "adding-feature", "Adding a New Feature" ],
-      [ "adding-suite", "Adding a Suite" ],
-      [ "writing-server-test", "Writing a Server Test" ],
-      [ "adding-dependency", "Adding a Dependency" ],
-    ]
-  end
+  # def guide_sections
+  #   [ [ "introduction", "Introduction" ],
+  #     [ "installing", "Installing Test Kitchen" ],
+  #     [ "getting-help", "Getting Help" ],
+  #     [ "creating-cookbook", "Creating a Cookbook" ],
+  #     [ "writing-recipe", "Writing a Recipe" ],
+  #     [ "running-converge", "Running Kitchen Converge" ],
+  #     [ "manually-verifying", "Manually Verifying" ],
+  #     [ "writing-test", "Writing a Test" ],
+  #     [ "running-verify", "Running Kitchen Verify" ],
+  #     [ "running-test", "Running Kitchen Test" ],
+  #     [ "adding-platform", "Adding a Platform"  ],
+  #     [ "fixing-converge", "Fixing Converge" ],
+  #     [ "adding-feature", "Adding a New Feature" ],
+  #     [ "adding-suite", "Adding a Suite" ],
+  #     [ "writing-server-test", "Writing a Server Test" ],
+  #     [ "adding-dependency", "Adding a Dependency" ],
+  #   ]
+  # end
 
-  def guide_index
-    page = File.basename(request["path"]).sub(%r{\.html$}, '')
-    guide_sections.index { |s| s.first == page }
-  end
+  # def guide_index
+  #   page = File.basename(request["path"]).sub(%r{\.html$}, '')
+  #   guide_sections.index { |s| s.first == page }
+  # end
 
-  def guide_progress
-    ((guide_index + 1.0) / guide_sections.size) * 100
-  end
+  # def guide_progress
+  #   ((guide_index + 1.0) / guide_sections.size) * 100
+  # end
 end
 
 set :css_dir, 'css'
