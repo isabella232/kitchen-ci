824eed37b2f91d19d13ef9b7ebbf253ee764166d
Fletcher Nichol <fnichol@nichol.ca>
2013-12-02 21:51:02 -0700

    Clean up Gemfile to remove deps, fix guide index issue.

--- Gemfile
+++ Gemfile
@@ -1,24 +1,16 @@
 source 'https://rubygems.org'
 
 gem "middleman"
-
-gem "slim"
-gem "pry"
+gem "middleman-livereload"
+gem "middleman-syntax", github: "middleman/middleman-syntax"
 
 gem "bootstrap-sass", github: "thomas-mcdonald/bootstrap-sass"
 
-gem "middleman-syntax", github: "middleman/middleman-syntax"
-
-gem "redcarpet"
-
-# Live-reloading plugin
-gem "middleman-livereload"
-
-# For faster file watcher updates on Windows:
-gem "wdm", "~> 0.1.0", :platforms => [:mswin, :mingw]
-
+gem "rack-contrib"
+gem "therubyracer"
 gem "thin"
 
 group :development do
   gem "git"
+  gem "pry"
 end
--- config.rb
+++ config.rb
@@ -1,30 +1,6 @@
-require "slim"
-
-###
-# Compass
-###
-
-# Change Compass configuration
-# compass_config do |config|
-#   config.output_style = :compact
-# end
-
 ###
 # Page options, layouts, aliases and proxies
 ###
-
-# Per-page layout changes:
-#
-# With no layout
-# page "/path/to/file.html", :layout => false
-#
-# With alternative layout
-# page "/path/to/file.html", :layout => :otherlayout
-#
-# A path which all have the same layout
-# with_layout :admin do
-#   page "/admin/*"
-# end
 
 with_layout :guide do
   page "/docs/getting-started/*"
@@ -42,64 +18,20 @@ end
 # activate :automatic_image_sizes
 
 # Reload the browser automatically whenever files change
-activate :directory_indexes
 activate :livereload
 
+activate :directory_indexes
+
 activate :syntax
-set :markdown_engine, :kramdown # explicitly stated even though this is the default
-# set :markdown, fenced_code_blocks: true, smartypants: true, highlight: true, tables: true # not sure if these options are needed w/ kramdown
+set :markdown_engine, :kramdown
 
 # Methods defined in the helpers block are available in templates
 helpers do
-  # def current_page?(page)
-  #   page.sub(%r{^/}, '') == request["path"]
-  # end
-
-  # def guide_nav(title, page)
-  #   path = "/getting-started/#{page}.html"
-
-  #   if current_page?(path)
-  #     %{<span class="glyphicon glyphicon-bookmark"></span> } + title
-  #   else
-  #     link_to(title, path)
-  #   end
-  # end
-
-  # def guide_sections
-  #   [ [ "introduction", "Introduction" ],
-  #     [ "installing", "Installing Test Kitchen" ],
-  #     [ "getting-help", "Getting Help" ],
-  #     [ "creating-cookbook", "Creating a Cookbook" ],
-  #     [ "writing-recipe", "Writing a Recipe" ],
-  #     [ "running-converge", "Running Kitchen Converge" ],
-  #     [ "manually-verifying", "Manually Verifying" ],
-  #     [ "writing-test", "Writing a Test" ],
-  #     [ "running-verify", "Running Kitchen Verify" ],
-  #     [ "running-test", "Running Kitchen Test" ],
-  #     [ "adding-platform", "Adding a Platform"  ],
-  #     [ "fixing-converge", "Fixing Converge" ],
-  #     [ "adding-feature", "Adding a New Feature" ],
-  #     [ "adding-suite", "Adding a Suite" ],
-  #     [ "writing-server-test", "Writing a Server Test" ],
-  #     [ "adding-dependency", "Adding a Dependency" ],
-  #   ]
-  # end
-
-  # def guide_index
-  #   page = File.basename(request["path"]).sub(%r{\.html$}, '')
-  #   guide_sections.index { |s| s.first == page }
-  # end
-
-  # def guide_progress
-  #   ((guide_index + 1.0) / guide_sections.size) * 100
-  # end
 end
 
-set :css_dir, 'css'
-
-set :js_dir, 'js'
-
-set :images_dir, 'img'
+set :css_dir,     'css'
+set :js_dir,      'js'
+set :images_dir,  'img'
 
 after_configuration do
   sprockets.append_path File.join(File.dirname(__FILE__), "vendor/css")
